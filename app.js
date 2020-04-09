9fbc4363a59a2a2d9a562e85c413e7a9d51fdb91
kristenmoran <kristen.moran30@gmail.com>
2020-04-08 21:13:10 -0400

    modal with instructions set up and functions with the start of styling applied

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log('js is up and running');
+
+//TARGET DATA
+
+//Modal
+const modal = document.querySelector('#modal-instructions');
+const openModalBtn = document.querySelector('#openModal');
+const closeModalBtn = document.querySelector('#closeModal');
+
+//EVENT LISTENERS
+
+//Modal
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+//FUNCTIONS
+
+//Modal
+function openModal() {
+	modal.style.display = 'block';
+}
+
+function closeModal() {
+	modal.style.display = 'none';
+}
