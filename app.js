4b5eefa0fc1614fd0166db5cf12d8dfd719ce73d
kristenmoran <kristen.moran30@gmail.com>
2020-04-09 12:56:33 -0400

    started a function to display question

--- app.js
+++ app.js
@@ -7,30 +7,34 @@ const modal = document.querySelector('#modal-instructions');
 const openModalBtn = document.querySelector('#openModal');
 const closeModalBtn = document.querySelector('#closeModal');
 
+//Questions Board
+const questionsBoard = document.querySelector('.question-board');
+const questionDisplay = document.querySelector('.question-display');
+
 //Music Category
 const music = document.querySelector('#music');
-
+//Objects in an array to hold the questions and answers
 const musicQuestions = [
 	{
-		question: 'Who composed the Four Seasons?',
-		choiceA: 'Wolfgang Amadeus Mozart',
-		choiceB: 'Johann Sebastian Bach',
-		choiceC: 'Antonio Vivaldi',
-		correctAnswer: 'Antonio Vivaldi',
+		question: '1. Who composed the Four Seasons?',
+		choiceA: 'A. Wolfgang Amadeus Mozart',
+		choiceB: 'B. Johann Sebastian Bach',
+		choiceC: 'C. Antonio Vivaldi',
+		correctAnswer: 'C',
 	},
 	{
-		question: 'What year was Abbey Road released?',
-		choiceA: '1971',
-		choiceB: '1969',
-		choiceC: '1967',
-		correctAnswer: '1969',
+		question: '2. What year was Abbey Road released?',
+		choiceA: 'A. 1971',
+		choiceB: 'B. 1969',
+		choiceC: 'C. 1967',
+		correctAnswer: 'B',
 	},
 	{
-		question: "What is Elton John's best selling single",
-		choiceA: 'Your Song',
-		choiceB: 'Candle in the Wind',
-		choiceC: 'Rocket Man',
-		correctAnswer: 'Candle in the Wind',
+		question: "3. What is Elton John's best selling single?",
+		choiceA: 'A. Your Song',
+		choiceB: 'B. Candle in the Wind',
+		choiceC: 'C. Rocket Man',
+		correctAnswer: 'B',
 	},
 ];
 
@@ -41,7 +45,7 @@ openModalBtn.addEventListener('click', openModal);
 closeModalBtn.addEventListener('click', closeModal);
 
 //Music Category
-music.addEventListener('click', startMusicTrivia);
+music.addEventListener('click', renderMusicQuestions);
 
 //FUNCTIONS
 
@@ -55,8 +59,31 @@ function closeModal() {
 }
 
 //Music Category
-function startMusicTrivia(event) {
-	//code goes here
+function renderMusicQuestions(event) {
 	event.target;
-	console.log('clicked!');
+	// console.log('clicked');
+	questionsBoard.style.display = 'block';
+	questionDisplay.style.fontFamily = 'Cabin, sans-serif';
+	questionDisplay.style.fontSize = '26px';
+	questionDisplay.style.textAlign = 'center';
+	//display 1 question and choices
+	questionDisplay.innerHTML = musicQuestions[0].question;
+	for (let i = 0; i < musicQuestions.length - 1; i++) {
+		console.log(i);
+		//after choice is made say if it is correct
+		let playAnswer = musicQuestions[i].correctAnswer;
+		if (playAnswer === musicQuestions[i].correctAnswer) {
+			console.log('correct');
+		} else {
+			//or not correct
+			console.log('incorrect');
+		}
+	}
 }
+
+// let questions = document.createElement('div');
+// console.log('made div');
+// questions.innerHTML = "questions.musicQuestions"
+// //go to next question
+// //repeat for question 2 and 3
+// //exit the game
